c0505dc5c1439fd9d7881faa317a1831e933fca2
Andro Valero <74332121+E-RI-CK@users.noreply.github.com>
2024-04-12 16:45:37 -0500

    Navigation component added

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navigation.css';
+
+export const Navigation = () => {
+
+    const [isActive, setActive] = useState(true)
+    const handleToggle = () => {
+        setActive(!isActive)
+    }
+
+    return (
+        <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+            <div className='container-fluid'>
+                <div className='logo'>
+                    <img src="/assets/star-wars.svg" alt="star wars logo" height={"75px"} width={"75px"}/>
+                </div>
+                <NavLink to='/' className='navbar-brand site-name'>
+                    Home
+                </NavLink>
+
+                {/* Toggler / hamburger button */}
+                <button
+                    onClick={handleToggle}
+                    className='navbar-toggler'
+                    type='button'
+                    data-bs-toggle='collapse'
+                    data-bs-target='#navbarColor01'
+                    aria-controls='navbarColor01'
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+
+                {/* Links  */}
+                <div
+                    className={`${isActive ? 'collapse' : ''} navbar-collapse`}
+                    id='navbarColor01'
+                >
+                    <ul className='navbar-nav me-auto'>
+                        <li className='nav-item'>
+                            <NavLink to='/characters/' className='nav-link'>
+                                Characters
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink to='/films/' className='nav-link'>
+                                Films
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,16 @@
+import '../App.css'
+import 'animate.css'
+import 'bootswatch/dist/lux/bootstrap.min.css'
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage/HomePage';
+import { Navigation } from '../components/Navigation/Navigation';
+
+
 
 export const AppRouter = () => {
     return (
         <div className='App'>
+            <Navigation />
             <Routes>
                 <Route path='/' element={<HomePage />} />
             </Routes>
